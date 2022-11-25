35aae7d6a5919d3b5889e53336bcc17ed33eaf65
Sahim98 <sahimsalem321@gmail.com>
2022-11-25 12:26:07 +0600

    lsjf

--- FlutterProject-main/flutter-application-main/test/widget_test.dart
+++ FlutterProject-main/flutter-application-main/test/widget_test.dart
@@ -6,12 +6,11 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Home.dart';
-import 'package:flutter_application_1/practice/backup1.dart';
+import 'package:flutter_application_1/emergency assistant/Home.dart';
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_application_1/main.dart';
+import 'package:flutter_application_1/emergency assistant/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
